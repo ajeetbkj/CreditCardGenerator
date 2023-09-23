6d6e2baa6ebd785025f0cda25ff13322e4104938
Ajeet kumar <Ajeet@Ajeets-MacBook-Air.local>
2023-09-23 22:41:29 +0530

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,231 @@
+"use strict";
+import "../sass/main.scss";
+import completeSvg from "../images/icon-complete.svg";
+
+const form = document.querySelector(".form");
+const inputName = document.getElementById("name");
+const inputCardNumber = document.getElementById("card-number");
+const inputExpMonth = document.querySelector(".exp-month");
+const inputExpYear = document.querySelector(".exp-year");
+const inputCvc = document.getElementById("cvc");
+
+const outputCardNumber = document.getElementById("output-card-number");
+const outputName = document.getElementById("output-name");
+const outputExpMonth = document.getElementById("output-month");
+const outputExpYear = document.getElementById("output-year");
+const outputCvc = document.getElementById("cvc-number");
+
+
+
+const renderError = function (status, element) {
+  const msgError = element.nextElementSibling;
+
+  switch (status) {
+    case "empty":
+      msgError.textContent = "Can't be blank";
+      element.style.borderColor = "var(--red)";
+      break;
+    case "NaN":
+      msgError.textContent = "Wrong format, numbers only";
+      element.style.borderColor = "var(--red)";
+      break;
+    case "invalid":
+      msgError.textContent = "Invalid date";
+      element.style.borderColor = "var(--red)";
+      break;
+    default:
+      msgError.textContent = "";
+      element.style.borderColor = null;
+  }
+};
+
+
+
+const spacingNums = function (value) {
+  const receivedVal = String(value);
+  const chunks = [];
+
+  for (let i = 0; i < receivedVal.length; i += 4) {
+    chunks.push(receivedVal.slice(i, i + 4));
+  }
+
+  const output = chunks.join(" ");
+  return output;
+};
+
+
+const validateCardNumber = function (element) {
+  const inputValue = +element.value.split(" ").join("");
+
+  if (inputCardNumber.value === "") {
+    renderError("empty", element);
+    outputCardNumber.textContent = "0000 0000 0000 0000";
+    return false;
+  }
+
+  if (isNaN(inputValue)) {
+    renderError("NaN", element);
+    return false;
+  }
+
+  const spacedValue = spacingNums(inputValue);
+  inputCardNumber.value = spacedValue;
+  outputCardNumber.textContent = spacedValue;
+
+  renderError("", element);
+  return true;
+};
+
+//Input
+inputCardNumber.addEventListener("input", function () {
+  validateCardNumber(inputCardNumber);
+});
+
+
+const validateName = function (element) {
+  if (inputName.value === "") {
+    renderError("empty", element);
+    outputName.textContent = "Your name";
+    return false;
+  }
+
+  renderError("", element);
+  outputName.textContent = inputName.value;
+  return true;
+};
+
+//Input
+inputName.addEventListener("input", function () {
+  validateName(inputName);
+});
+
+
+const validateMonth = function (element) {
+  const inputValue = element.value;
+  if (inputValue === "") {
+    renderError("empty", element);
+    outputExpMonth.textContent = "00";
+    return false;
+  }
+
+  if (isNaN(inputValue) || inputValue < 1 || inputValue > 12) {
+    renderError("invalid", element);
+    return false;
+  }
+
+  renderError("", element);
+  outputExpMonth.textContent =
+    inputValue.length === 1 ? `0${inputValue}` : inputValue;
+
+  return true;
+};
+
+//Input
+inputExpMonth.addEventListener("input", function () {
+  validateMonth(inputExpMonth);
+});
+
+
+const validateYear = function (element) {
+  const inputValue = element.value;
+
+  if (inputValue === "") {
+    renderError("empty", element);
+    outputExpYear.textContent = "00";
+    return false;
+  }
+
+  if (isNaN(inputValue)) {
+    renderError("invalid", element);
+    return false;
+  }
+
+  renderError("", element);
+  outputExpYear.textContent =
+    inputValue.length === 1 ? `0${inputValue}` : inputValue;
+
+  return true;
+};
+
+//Input
+inputExpYear.addEventListener("input", function () {
+  validateYear(inputExpYear);
+});
+
+
+const validateCvc = function (element) {
+  const inputValue = element.value;
+
+  if (inputValue === "") {
+    renderError("empty", element);
+    outputCvc.textContent = "000";
+    return false;
+  }
+
+  if (isNaN(inputValue)) {
+    renderError("invalid", element);
+    return false;
+  }
+
+  const formate = function (value) {
+    if (value.length === 1) return `00${value}`;
+    if (value.length === 2) return `0${value}`;
+    return value;
+  };
+
+  renderError("", element);
+  outputCvc.textContent = formate(inputValue);
+  return true;
+};
+
+//Input
+inputCvc.addEventListener("input", function () {
+  validateCvc(inputCvc);
+});
+
+
+
+const renderThankYou = function () {
+  const markup = `
+    <div class="completed">
+      <img src="${completeSvg}" alt="Completed check icon" />
+
+      <p class="completed__greeting">Thank you!</p>
+      <p class="completed__message">We've added your card details</p>
+      <button class="completed__btn-continue button" type="button">
+        Continue
+      </button>
+    </div>
+  `;
+
+  form.style.opacity = 0;
+
+  setTimeout(() => {
+    form.innerHTML = markup;
+    form.style.opacity = 1;
+    const btnContinue = document.querySelector(".completed__btn-continue");
+    btnContinue.addEventListener("click", function () {
+      location.reload();
+    });
+  }, 300);
+};
+
+
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  renderThankYou();
+};
+
+form.addEventListener("submit", function (event) {
+  if (
+    validateName(inputName) &&
+    validateCardNumber(inputCardNumber) &&
+    validateMonth(inputExpMonth) &&
+    validateYear(inputExpYear) &&
+    validateCvc(inputCvc)
+  ) {
+    handleSubmit(event);
+  }
+});
